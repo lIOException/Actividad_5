1c3fae5328939ea806f73cd30d9ef71da4370926
Leonardo Arias <leonard-ar@hotmail.com>
2022-09-06 19:18:47 -0500

    Se agrega la actividad

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        double x = 3;
+        double y = 9;
+        System.out.println(x + " + " + y + " = " + (x+y));
+        System.out.println(x + " - " + y + " = " + (x-y));
+        System.out.println(x + " * " + y + " = " + (x*y));
+        System.out.println(x + " / " + y + " = " + (x/y));
+        System.out.println(x + " % " + y + " = " + (x%y));
+    }
+}
